01c358e45a1b04ff23f9d618743be14aac95e739
editionsHM <editionheritagemohammadien@gmail.com>
2025-12-30 21:09:59 +0100

    modified:   components/HeroCarousel.tsx

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -41,40 +41,15 @@ export function HeroCarousel() {
     )
 
     return (
-        <div className="w-full h-full min-h-[400px] lg:min-h-[500px] group relative">
-            <Carousel
-                plugins={[plugin.current]}
-                className="w-full h-full rounded-2xl overflow-hidden shadow-2xl ring-1 ring-slate-100"
-                onMouseEnter={plugin.current.stop}
-                onMouseLeave={plugin.current.reset}
-                opts={{
-                    loop: true,
-                }}
-            >
-                <CarouselContent className="h-full">
-                    {carouselImages.map((image, index) => (
-                        <CarouselItem key={index} className="h-full">
-                            <div className="relative h-full w-full bg-slate-50">
-                                <Image
-                                    src={image.src}
-                                    alt={image.alt}
-                                    fill
-                                    className="object-cover"
-                                    priority={index === 0}
-                                    quality={100}
-                                />
-                            </div>
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
-                {/* Navigation Arrows - Visible on hover or always on desktop */}
-                <div className="absolute top-1/2 left-4 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                    <CarouselPrevious className="relative left-0 translate-y-0 h-10 w-10 border-none bg-black/50 hover:bg-black text-white" />
-                </div>
-                <div className="absolute top-1/2 right-4 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                    <CarouselNext className="relative right-0 translate-y-0 h-10 w-10 border-none bg-black/50 hover:bg-black text-white" />
-                </div>
-            </Carousel>
+        <div className="w-full h-full min-h-[400px] lg:min-h-[500px] group relative rounded-2xl overflow-hidden shadow-2xl ring-1 ring-slate-100 border-2 border-red-500">
+            <Image
+                src="/images/carousel-4.jpg"
+                alt="Test Image"
+                fill
+                className="object-cover"
+                priority
+                unoptimized
+            />
         </div>
     )
 }
